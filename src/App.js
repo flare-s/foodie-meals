bf05865cd866fcc0b07aa05f6e9a5b62aa1666c8
flare-s <webflarez@gmail.com>
2022-09-03 13:20:25 +0100

    Added meals summary section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import MealsSummary from "./components/Meals/MealsSummary/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+      </main>
     </Fragment>
   );
 }
