eb0f14abd49fcf84410a5a197b0b6d0495bfd1aa
flare-s <webflarez@gmail.com>
2022-09-04 22:09:37 +0100

    Added cart button animation

--- src/components/Cart/CartButton/CartButton.js
+++ src/components/Cart/CartButton/CartButton.js
@@ -1,15 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartIcon from "../CartIcon";
 import styles from "./CartButton.module.scss";
 import { cartContext } from "../../../store/CartContext";
 const CartButton = (props) => {
+  const [isAnimated, setIsAnimated] = useState(false);
+  const btnClass = `${styles.button} ${isAnimated ? styles.bump : ""}`;
   const { cartState } = useContext(cartContext);
+
+  useEffect(() => {
+    if (cartState.items.length < 1) return;
+
+    setIsAnimated(true);
+
+    const timer = setTimeout(() => {
+      setIsAnimated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartState.items]);
+
   const currentCartItems = cartState.items.reduce(
     (accumulator, item) => accumulator + item.amount,
     0
   );
   return (
-    <button className={styles.button} onClick={props.closeModal}>
+    <button className={btnClass} onClick={props.closeModal}>
       <CartIcon className={styles.icon} />
       Cart
       <span className={styles.badge}>{currentCartItems}</span>
